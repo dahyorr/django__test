153150dc54fe7a7c88839d148d6edaf71c7bbe1b
Dayo Adebanjo <43008594+dah-yor@users.noreply.github.com>
2020-05-24 00:51:35 +0100

    deleted venv

--- auth_tutorial/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\dahyo\PycharmProjects\django_tutorial\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
